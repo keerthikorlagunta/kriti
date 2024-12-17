275d2a091dc41192950ee785dc081abb024f9359
keerthikorlagunta <keerthikorlagunta@gmail.com>
2024-12-18 04:20:33 +0530

    number

---
+++ start.py
@@ -0,0 +1,9 @@
+a=int(input('enter char:'))
+if 'A'<=a<='Z':
+    print('upper case')
+elif 'a'<=a<='z':
+    print('lower case')
+elif '0'<=a<='9':
+    print('numbers')
+else:
+    print('special char')
